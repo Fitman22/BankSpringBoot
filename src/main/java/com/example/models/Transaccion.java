ccb4493cc30fd55cd0f895aee8d32d81c2133993
Fitman22 <85381077+Fitman22@users.noreply.github.com>
2024-10-17 22:37:12 -0500

    transacciones

--- src/main/java/com/example/models/Transaccion.java
+++ src/main/java/com/example/models/Transaccion.java
@@ -28,11 +28,11 @@ public class Transaccion {
 
     // Getters y setters
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
