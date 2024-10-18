6a317bda1414ffe26eaa53c698792d7d1cf1ef7d
Fitman22 <85381077+Fitman22@users.noreply.github.com>
2024-10-17 20:43:13 -0500

    transacciones

--- src/main/java/com/example/models/Cuenta.java
+++ src/main/java/com/example/models/Cuenta.java
@@ -25,5 +25,52 @@ public class Cuenta {
     @OneToMany(mappedBy = "cuentaDestino")
     private List<Transaccion> transaccionesDestino;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Transaccion> getTransaccionesOrigen() {
+        return transaccionesOrigen;
+    }
+
+    public void setTransaccionesOrigen(List<Transaccion> transaccionesOrigen) {
+        this.transaccionesOrigen = transaccionesOrigen;
+    }
+
+    public List<Transaccion> getTransaccionesDestino() {
+        return transaccionesDestino;
+    }
+
+    public void setTransaccionesDestino(List<Transaccion> transaccionesDestino) {
+        this.transaccionesDestino = transaccionesDestino;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
 
